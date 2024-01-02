64cccb3a3400fde2dc832a05abb2f5eaf5cf0e6d
polyhedra <kostyria.m@gmail.com>
2024-01-02 20:43:02 +0200

    Initial Commit

---
+++ SwiftUI Experiments/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Experiments
+//
+//  Created by Mykhailo on 1/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI Experiments/SwiftUI_ExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExperimentsApp.swift
+//  SwiftUI Experiments
+//
+//  Created by Mykhailo on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
